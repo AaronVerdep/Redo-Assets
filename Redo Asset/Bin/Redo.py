98b9dd22e5843c43fa812b3170fe35afdb7e49e0
AaronVerdep <139834084+AaronVerdep@users.noreply.github.com>
2023-08-14 21:54:37 -0400

    Redo
    Use the file for play Redo

---
+++ Redo Asset/Bin/Redo.py
@@ -0,0 +1,7 @@
+import time
+
+print("Starting game...")
+time.sleep(5)
+print("Downloading Resources...")
+time.sleep(6)
+print("Uhhhh, Sorry, But need to download the Source Code!, Sorry, My Bad.")
